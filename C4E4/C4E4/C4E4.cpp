0dc2269a231e66ce4741680a741f4798d63f914e
Saviour Lee <OoSaviouroO@hotmail.com>
2017-10-12 21:34:52 +0800

    C4E4

--- C4E4/C4E4/C4E4.cpp
+++ C4E4/C4E4/C4E4.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void copy_n(char dst[], char src[], int n) {
+	int i = 0;
+	while (i<n && src[i] != '\0') {
+			dst[i] = src[i];
+			++i;
+	}
+	if (i < n) {
+		for (int j = i; j < n; j++)
+			dst[i] = '\0';
+	}
+}
 
 int main()
 {
+	char d[20];
+	char s[20] = "Hello World!";
+	copy_n(d, s, 10);
+	printf("%s", d);
     return 0;
 }
 
